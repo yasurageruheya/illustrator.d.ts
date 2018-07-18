c039b00714cf88b3358d58cbf2f58e35c553637c
毛 <kdjn6223@gmail.com>
2018-07-19 05:54:05 +0900

    collection object が配列としてもコードヒントが効くように修正

--- Variable.d.ts
+++ Variable.d.ts
@@ -38,12 +38,12 @@ declare class Variable
 	/**
 	 * Read-only. All of the artwork in the variable.
 	 * @const
-	 * @type {PageItems} {@link PageItems} collection object
+	 * @type {PageItems|PageItem[]} {@link PageItems} collection object
 	 * @see {@link Variable}
 	 * @since CS2
 	 * @version CS2
 	 */
-	readonly pageItems:PageItems;
+	readonly pageItems:PageItems|PageItem[];
 
 	/**
 	 * Read-only. The object that contains the variable.
